e5f82b779ece7525542ccab31c903a276f344bb2
Matt Winzer <matt.winzer@gmail.com>
2018-10-24 00:08:11 -0600

    controlled ComposeMessage form inputs

--- src/components/ComposeMessage.js
+++ src/components/ComposeMessage.js
@@ -6,6 +6,12 @@ class ComposeMessage extends Component {
     body: ''
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    }, () => console.log(this.state))
+  }
+
   render() {
     return (
       <form className="form-horizontal well">
@@ -17,13 +23,13 @@ class ComposeMessage extends Component {
         <div className="form-group">
           <label htmlFor="subject" className="col-sm-2 control-label">Subject</label>
           <div className="col-sm-8">
-            <input type="text" className="form-control" id="subject" placeholder="Enter a subject" name="subject" />
+            <input type="text" className="form-control" id="subject" placeholder="Enter a subject" name="subject" onChange={this.handleChange} value={this.state.subject} />
           </div>
         </div>
         <div className="form-group">
           <label htmlFor="body" className="col-sm-2 control-label">Body</label>
           <div className="col-sm-8">
-            <textarea name="body" id="body" className="form-control"></textarea>
+            <textarea name="body" id="body" className="form-control" onChange={this.handleChange} value={this.state.body}></textarea>
           </div>
         </div>
         <div className="form-group">
